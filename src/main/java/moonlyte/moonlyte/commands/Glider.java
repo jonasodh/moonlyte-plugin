94b8c21f26d9f4c4412c14305a7ed2e30c71be59
Jonas Østergaard den Hollander <jonasodh@hotmail.com>
2021-12-19 16:13:47 +0100

    Added toggle glider for other players

--- src/main/java/moonlyte/moonlyte/commands/Glider.java
+++ src/main/java/moonlyte/moonlyte/commands/Glider.java
@@ -1,5 +1,6 @@
 package moonlyte.moonlyte.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -17,22 +18,47 @@ public class Glider implements CommandExecutor {
         if (sender instanceof Player) {
             Player p = (Player) sender;
             ItemStack pInv = p.getInventory().getChestplate();
+
             if (p.hasPermission("moonlyte.glide")) {
-                if ((pInv == null) || !(pInv.getType() == Material.ELYTRA)) {
-                    if (!gliderList.contains(p)) {
-                        gliderList.add(p);
-                        p.setGliding(true);
-                        p.sendMessage("Glider on");
+                if (args.length == 0) {
+                    if ((pInv == null) || !(pInv.getType() == Material.ELYTRA)) {
+                        if (!gliderList.contains(p)) {
+                            gliderList.add(p);
+                            p.setGliding(true);
+                            p.sendMessage("Glider on");
+                        } else {
+                            gliderList.remove(p);
+                            p.setGliding(false);
+                            p.sendMessage("Glider off");
+                        }
                     } else {
-                        gliderList.remove(p);
-                        p.setGliding(false);
-                        p.sendMessage("Glider off");
+                        p.sendMessage("You can not wear an elytra and glide at the same time.");
                     }
-                } else {
-                    p.sendMessage("You can not wear an elytra and glide at the same time.");
                 }
             } else {
                 p.sendMessage("You don't have permission for this command.");
+            }
+
+            if (p.hasPermission("moonlyte.glide.others")) {
+                if (args.length > 0) {
+                    Player target = Bukkit.getPlayerExact(args[0]);
+                    ItemStack tInv = target.getInventory().getChestplate();
+                    if ((tInv == null) || !(tInv.getType() == Material.ELYTRA)) {
+                        if (!gliderList.contains(target)) {
+                            gliderList.add(target);
+                            target.setGliding(true);
+                            target.sendMessage("Glider on");
+                            p.sendMessage("Turned " + target.getDisplayName() + " into a glider.");
+                        } else {
+                            gliderList.remove(target);
+                            target.setGliding(false);
+                            target.sendMessage("Glider off");
+                            p.sendMessage("Turned off gliding for " + target.getDisplayName());
+                        }
+                    } else {
+                        p.sendMessage(target.getDisplayName() + " is wearing an elytra so gliding was not enabled.");
+                    }
+                }
             }
         }
         return true;
